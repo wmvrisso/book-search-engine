0463bde0a26cd707c0d0eec01b23bb1e2fb3c0c8
wmvrisso <wmvrisso@gmail.com>
2025-04-14 19:40:40 -0700

    updated server.ts file

--- Develop/server/src/server.ts
+++ Develop/server/src/server.ts
@@ -1,41 +1,43 @@
-import express from 'express';
-import path from 'node:path';
-import db from './config/connection.js';
-import { ApolloServer } from '@apollo/server';// Note: Import from @apollo/server-express
-import { expressMiddleware } from '@apollo/server/express4';
-import { typeDefs, resolvers } from './schemas/index.js';
-import { authenticateToken } from './utils/auth.js';
+import express from "express";
+import path from "node:path";
+import db from "./config/connection.js";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import { typeDefs, resolvers } from "./schemas/index.js";
+import { authenticateToken } from "./utils/auth.js";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
 });
 
 const startApolloServer = async () => {
   await server.start();
   await db();
 
-app.use('/graphql', expressMiddleware(server as any,
-  {
-    context: authenticateToken as any
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  app.use(
+    "/graphql",
+    expressMiddleware(server as any, {
+      context: async ({ req }) => {
+        const token = req.headers.authorization || "";
+        const user = authenticateToken(token);
+        return { user };
+      },
+    })
+  );
+
+  // if we're in production, serve client/build as static assets
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/build")));
   }
-));
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-
 
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-
-}
+};
 
 startApolloServer();
