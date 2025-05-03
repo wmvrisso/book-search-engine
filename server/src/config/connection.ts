35626f3f14d597e56efc2846436b4058899ece31
wmvrisso <wmvrisso@gmail.com>
2025-05-03 16:43:50 -0700

    updated connection.ts file

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -1,18 +1,18 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI || '';
+const MONGODB_URI = process.env.MONGODB_URI || "";
 
 const db = async (): Promise<typeof mongoose.connection> => {
   try {
     await mongoose.connect(MONGODB_URI);
-    console.log('Database connected.');
+    console.log("Database connected.");
     return mongoose.connection;
   } catch (error) {
-    console.error('Database connection error:', error);
-    throw new Error('Database connection failed.');
+    console.error("Database connection error:", error);
+    throw new Error("Database connection failed.");
   }
 };
 
